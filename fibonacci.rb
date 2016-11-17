c3922ce261a95aabe054d1071b5b8953a307410d
rushlinneu <rushlinneu@gmail.com>
2016-11-17 15:32:56 +1300

    add fibonacci

---
+++ fibonacci.rb
@@ -0,0 +1,13 @@
+def fibs(n)
+	cur, nxt = 1, 1
+	result = []
+	n.times do
+		result << cur
+		cur, nxt = nxt, cur+nxt
+	end
+	return result
+end
+
+def fibs_rec(n, lst=[1, 1])
+	n < 3? lst : fibs_rec(n-1, lst+[lst[-1]+lst[-2]])
+end
